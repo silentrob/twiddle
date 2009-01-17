a27ae5f4d1315a216dfdfbd5e6805fbb09d7d6ef
Brian LeRoux <brian.leroux@westcoastlogic.com>
2009-01-17 14:53:42 -0800

    looking good but tricky!

--- src/assets/js/controllers/auth.js
+++ src/assets/js/controllers/auth.js
@@ -1,4 +1,17 @@
 var Auth = {
-	'_login.html':function(){},
-	'_account.html':function(){}
+	
+	'_login.html':function(){
+		// initialize the form to auth the person 
+		x$('form').submit(function(e){
+			Person.username = x$('#username');
+			Person.password = x$('#password');
+			// TODO real twitter auth..
+			// redirect them to the twiddle home
+			app.render('twiddle/_index.html')
+		});
+	},
+	
+	'_account.html':function(){
+		// TODO fetch the user info for display
+	}
 };
--- src/assets/js/controllers/twiddle.js
+++ src/assets/js/controllers/twiddle.js
@@ -1,10 +1,23 @@
 var Twiddle = {
+		
+	// fetches recent tweets
 	'_index.html':function(){
-		// fetches recent tweets
-		x$('#tweets').xhrjson('/twitter/', {partial:'_tweet.html', map:{} });
+
+		// for now lets just fetch the public timeline json
+		var url = 'http://twitter.com/statuses/public_timeline.json'
 		
-		// enables click to tweet
+		// the returned json is an array of objs
+		// keys are the json paths to the data
+		// values are the dom path to where we insert the data
+		var partialMap = {
+			'user/profile_image_url':'img.avatar[@src]',
+			'user/name':'div.username',
+			'text':'div.message'
+		};
 		
-		// enables refresh click
+		x$('#tweets').xhrjson( url, {partial:'_tweet.html', map:partialMap });
+		
+		// TODO click to tweet
+		// TODO refresh
 	}
 };
