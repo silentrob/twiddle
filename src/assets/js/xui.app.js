e601144ef4688363ebb3a497b39f4c84343ee8b3
Rob Ellis <kazoomer@gmail.com>
2009-01-17 20:09:30 -0800

    Fixed things in parseDefaults

--- src/assets/js/xui.app.js
+++ src/assets/js/xui.app.js
@@ -1,59 +1,66 @@
 x$.app = function(title, config) {
 
 	// private helper for loading configuration and defaults
-	var parseDefaults = function( cfg ) {
-		var choose = function( given, default ) { return( given == undefined ? default : given )};
+	
+	 var parseDefaults = function( cfg ) {
+	
+		var choose = function( given, def ) { return (given == undefined) ? def : given; }
+
 		return {
 			// application filters
-			after: 	     choose( cfg.after, function(){console.log('app.config.after called')}),
-			before:      choose( cfg.before, function(){console.log('app.config.before called')},
-			// layouts and templating
-			layout:      choose( cfg.layout, 'index.html'),  // default layout used
-			container:   choose( cfg.container, '#content'), // default element action results will be rendered in
-			index: 	   	 choose( cfg.index, '_index.html'),  // default action to load
-			// controllers and models
-			controllers: choose( cfg.controllers, []),
-			models: 	 choose( cfg.models, [])
+			 after: 	     choose( cfg.after, function(){console.log('app.config.after called')}),
+			 before:      choose( cfg.before, function(){console.log('app.config.before called')}),
+			 // layouts and templating
+			 layout:      choose( cfg.layout, 'index.html'),  // default layout used
+			 container:   choose( cfg.container, '#content'), // default element action results will be rendered in
+			 index: 	   	 choose( cfg.index, '_index.html'),  // default action to load
+			 // controllers and models
+			 controllers: choose( cfg.controllers, []),
+			 models: 	 choose( cfg.models, [])
 		}
+	
 	};
-	
-	// the framework
-	var app = {
-		
-		// setup the defaults
-		config:parseDefaults(config),
-		
-		// array of actions loaded
-		history:[],
-		
-		// renders an action
-		render: function(url) {	
-			document.location.hash = u = url.split('/').pop();
 
-			// not sure what this chunk is doing?
-			for(var action in config) {		
-				if(typeof action == 'string' && new RegExp(u).test(action))
-					config[action]();
-			};
-
-			x$(app.config.container).xhr( url,{ callback:function(){
-				x$(app.config.container).html(this.responseText);
-
-				x$('.nav A').click(function(e) { 
-					e.preventDefault();
-					app.render(this.href);
-					x$('#back').click(function(){ app.render(app.history.pop() )});
-				});
-			}});
-		}
-	};
-	
-	
-	// initialize the whole shebang
-	x$(window).load(function(){
-		app.config.before();
-		app.history.push(app.config.index);
-		app.render( document.location.hash.replace("#",'') || app.config.index );
-		app.config.after();
-	});
+	console.log(parseDefaults(config));
+	// 
+	// // the framework
+	// var app = {
+	// 	
+	// 	// setup the defaults
+	// 	config:parseDefaults(config),
+	// 	
+	// 	// array of actions loaded
+	// 	history:[],
+	// 	
+	// 	// renders an action
+	// 	render: function(url) {	
+	// 
+	// 		document.location.hash = u = url.split('/').pop();
+	// 
+	// 		// not sure what this chunk is doing?
+	// 		for(var action in config) {		
+	// 			if(typeof action == 'string' && new RegExp(u).test(action))
+	// 				config[action]();
+	// 		};
+	// 
+	// 		x$(app.config.container).xhr( url,{ callback:function(){
+	// 			x$(app.config.container).html(this.responseText);
+	// 
+	// 			x$('.nav A').click(function(e) { 
+	// 				e.preventDefault();
+	// 				app.render(this.href);
+	// 				x$('#back').click(function(){ app.render(app.history.pop() )});
+	// 			});
+	// 		}});
+	// 	}
+	// };
+	// 
+	// 
+	// // initialize the whole shebang
+	// x$(window).load(function(){
+	// 	//app.config.before();
+	// 	//app.history.push(app.config.index);
+	// 	//app.render( document.location.hash.replace("#",'') || app.config.index );
+	// 	//app.config.after();
+	// });
 };
