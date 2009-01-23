8aef34721c640180ae4a5357e18097977ecbb836
Rob Ellis <kazoomer@gmail.com>
2009-01-24 02:56:01 +0800

    Trying to get xhr's to fire in the right order
    Signed-off-by: Brian LeRoux <brian@westcoastlogic.com>

--- src/assets/js/controllers/twiddle.js
+++ src/assets/js/controllers/twiddle.js
@@ -4,70 +4,72 @@ var Twiddle = x$.controller('twiddle', {
 	'_index.html':function(){
 		
 		// for now lets just fetch the public timeline json
-		var url = 'http://twitter.com/statuses/public_timeline.json'
-		
+		var url = 'http://twitter.com/statuses/public_timeline.json' + '?q=' +  Math.random() 
+
     	var callback = function() {
+	       	console.log('step 1');
+/*	
+			var js = eval('(' + this.responseText + ')');
+			
 			// Get the partial in an internal XHR
-			var partial = null;
 			if (options.partial) {
+
+	
 				var cb2 = function() {
-					partial = this.responseText;
-				}
-				x$('#tweets').xhr(options.partial,{callback:cb2});
-			}
-			
-			// xui-app-tmp
-			x$('#template').html(partial);
-			
-			// -- where we need to do dom insertion	
-			var createDefaultsElement = function(html) {
-				var extn = html.split('.').pop();
-				var valid = ['jpg','jpeg','png','gif'];
-				for( var i = 0; i < valid.length; i++ ) {
-					if( valid[i] == extn ) {
-						var img = new Image();
-						img.src = html;
-						return img;
+	       			console.log('step 2');
+					var part = this.responseText;
+					
+					// -- where we need to do dom insertion	
+					var createDefaultsElement = function(html) {
+						var extn = html.split('.').pop();
+						var valid = ['jpg','jpeg','png','gif'];
+						for( var i = 0; i < valid.length; i++ ) {
+							if( valid[i] == extn ) {
+								var img = new Image();
+								img.src = html;
+								return img;
+							}
+						}
+						return html;
+					}
+					
+					// xui-app-tmp
+					x$('#template').html(part);
+
+					// if the json returns an array we're going to loop and create multiple partial
+	
+					var len = js.length;
+					// first we loop thru the json array
+					for( var i = 0; i < len; i++ ) {
+						// then we loop thru our partial map
+						for (var j in options.partialMap) {
+							var nodeKey = j;
+							var selector = options.partialMap[j];
+							var val = jsonPath(js[i],'$.'+nodeKey)[0];
+							x$('#template ' + selector).html(createDefaultsElement(val));
+						}
+						// clone partial here....
+						var temp = x$('#template .tweet').first();
+						var dup = temp.cloneNode(true);
+						x$("#tweets").html('bottom',dup);
+
 					}
 				}
-				return html;
-			}
-			// if the json returns an array we're going to loop and create multiple partials
-         	var js = eval('(' + this.responseText + ')');
-			var len = js.length;
-			// first we loop thru the json array
-			for( var i = 0; i < len; i++ ) {
-				// then we loop thru our partial map
-				for (var j in options.partialMap) {
-					var nodeKey = j;
-					var selector = options.partialMap[j];
-					var val = jsonPath(js[i],'$.'+nodeKey)[0];
-					x$('#template ' + selector).html(createDefaultsElement(val));
-				}
-				// clone partial here....
-				var temp = x$('#template .tweet').first();
-				var dup = temp.cloneNode(true);
-				x$("#tweets").html('bottom',dup);
-				
-			}
-			// if the json returns on obj we just insert as per the map into a single obj
+				x$('#tweets').xhr(options.partial,{callback:cb2});
+			}	
+			*/
 		};
 		
-		
-		
-		//----------------
-		
-		var options = {};	  	
-		options.callback = callback;
-	  	options.partial = 'assets/html/twiddle/_tweet.html';
 
-		options.partialMap = {
-			"user.profile_image_url":'.avatar',
-			"user.name":'.username',
-			"text":'.message'
-		};
-
-		x$('#tweets').xhr('assets/html/data.json',options);
+		x$('#tweets').xhr(url,{
+			callback: function() { console.log('here'); },
+		  	partial:'assets/html/twiddle/_tweet.html',
+			partialMap: {
+				"user.profile_image_url":'.avatar',
+				"user.name":'.username',
+				"text":'.message'
+			}
+		});
 		
 		//-------------------
 		
